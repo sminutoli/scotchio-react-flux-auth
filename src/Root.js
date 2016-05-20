9249be887902d4901df5ed7ced674a42ef799369
sminutoli <sminutoli.it@gmail.com>
2016-05-20 20:55:02 -0300

    Makes the router a statless component. Makes the ContactDetail a stateful component accesing to the store

--- src/Root.js
+++ src/Root.js
@@ -1,27 +1,19 @@
 // Root.js
 
-import React, { Component } from 'react';
+import React from 'react';
 import { Router, Route, IndexRoute } from 'react-router';
 import Index from './components/Index';
 import ContactDetail from './components/ContactDetail';
 
 import App from './components/Main';
 
-class Root extends Component {
-
-  // We need to provide a list of routes
-  // for our app, and in this case we are
-  // doing so from a Root component
-  render() {
-    return (
-      <Router history={this.props.history}>
-        <Route path='/' component={App}>
-          <IndexRoute component={Index}/>
-          <Route path='/contact/:id' component={ContactDetail} />
-        </Route>
-      </Router>
-    );
-  }
-}
-
+// We need to provide a list of routes
+// for our app, and in this case we are
+// doing so from a Root component
+const Root = ({history}) => <Router history={history}>
+  <Route path='/' component={App}>
+    <IndexRoute component={Index}/>
+    <Route path='/contact/:id' component={ContactDetail} />
+  </Route>
+</Router>;
 export default Root;
